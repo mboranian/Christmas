7b0a348c8ee53c5777914c42817a9655a96f4d6a
Matthew Boranian <matthewboranian@Matthews-MacBook-Air.local>
2025-10-24 19:22:22 -0700

    Remove useless constructor to fix ESLint warning in CI build

--- src/utils/gistStorage.ts
+++ src/utils/gistStorage.ts
@@ -23,10 +23,6 @@ interface GistResponse {
 class GitHubGistStorage {
   private gistId: string = GIST_ID;
 
-  constructor() {
-    // Use the hardcoded gist ID - no user setup needed
-  }
-
 
 
   private async updateGist(gistId: string, data: GistData): Promise<void> {
